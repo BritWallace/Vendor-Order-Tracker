960203007113270b89a48d12bfa00cffde0466d1
Brit Wallace <britwallace888@gmail.com>
2022-02-24 23:08:37 -0800

    first commit

---
+++ VendorOrder/Program.cs
@@ -0,0 +1,13 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace VendorandOrderTracker
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+  
+    }
+  }
+}
