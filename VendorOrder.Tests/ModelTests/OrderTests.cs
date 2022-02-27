10ffc6fd2edc20f10d60aab63a1f758f9c9443d1
Brit Wallace <britwallace888@gmail.com>
2022-02-26 16:20:43 -0800

    Test passes Order class constructor

--- VendorOrder.Tests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -1,5 +1,30 @@
 
-  using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using VendorOrder.Models;
+
+namespace VendorOrder.Tests
+{
+    [TestClass]
+  public class OrderTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("NameVender", "tests description", 100, "tests date");
+
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
+
+  // [TestMethod]
+  
+  // {public void OrderConstructor_CreatesInstanceOfOrder_Order()
+  //   Order newOrder = new Order();
+  // }
--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -4,5 +4,27 @@ using System;
 using VendorOrder.Models;
 namespace VendorOrder.Tests
 {
-  
+  [TestClass]
+  public class VendorTests
+  {
+    
+  }
+  // public class VendorTests : IDisposable
+  // {
+  //   public void Dispose()
+  // }
+  // Vendor.ClearAll();
 }
+  // [TestClass]
+  //   public class VendorTests
+  //   {
+  //   public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+  //   {
+  //     Vendor newVendor = new Vendor("Yo");
+  //     Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+  //   }
+    
+
+
+
+
